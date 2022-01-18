aba5896f18ffc9d7e0a4dae8ef49304b5a18dada
riroan <riroan@naver.com>
2022-01-18 23:55:14 +0900

    add self_play method

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 import numpy as np
+from agent import Agent
 
 class Game:
     def __init__(self, cfg, rule):
         self.cfg = cfg
         self.rule = rule
 
-        self.color = cfg.BLACK
+        self.init()
+
+    def init(self):
+        self.color = self.cfg.BLACK
         self.board = np.zeros((self.cfg.BOARD_SIZE, self.cfg.BOARD_SIZE))
         self.over = False
         self.num = 0
@@ -19,3 +23,21 @@ class Game:
         self.num += 1
         self.color *= -1
         return True
+
+    def self_play(self):
+        agent_b = Agent(self.cfg.BLACK, self.cfg)
+        agent_w = Agent(self.cfg.BLACK, self.cfg)
+        while not self.over:
+            if self.color == self.cfg.BLACK:
+                act = agent_b.get_action(self.board)
+            else:
+                act = agent_w.get_action(self.board)
+            
+            if self.action(act):
+                done, reward = self.rule.end_check(self.board)
+                self.over = done
+                print(self.board)
+        if reward == self.cfg.BLACK:
+            print("black win")
+        else:
+            print("white win")
