d4ac2e972c115c269cc126216cea7a153682d040
riroan <riroan@naver.com>
2022-01-20 18:16:57 +0900

    add train params

--- config.py
+++ config.py
@@ -8,17 +8,24 @@ class Config:
         self.tau = 10
         self.c_puct = 5
         self.decay = 0.95
+        self.split_rate = 0.95
 
+        # pygame config
         self.SPACE = 30
         self.STONE_SIZE = 15
         self.IN_MARGIN = 20
         self.OUT_MARGIN = 30
         self.MARGIN = self.IN_MARGIN+self.OUT_MARGIN
-        self.size = [2*self.MARGIN+self.SPACE*(self.BOARD_SIZE-1),2*self.MARGIN+self.SPACE*(self.BOARD_SIZE-1)]
+        self.window_size = [2*self.MARGIN+self.SPACE*(self.BOARD_SIZE-1),2*self.MARGIN+self.SPACE*(self.BOARD_SIZE-1)]
         self.BLACK_COLOR = (  0,  0,  0)
         self.WHITE_COLOR = (255,255,255)
         self.BOARD_COLOR = (252,191,146)
         self.FPS = 10
+        self.LEFT = 1
+
+        self.TRAIN_EPOCH = 3
+        self.BATCH_SIZE = 4
+        
         self.BOARD_SIZE = 15
         self.STONE_MAX = 5
         self.NONE = 0
@@ -26,8 +33,6 @@ class Config:
         self.WHITE = -1
 
         self.coef = 1e-4
-        self.LEFT = 1
-
         self.maxlen = 100
 
     def index2coordinate(self, index):
